9a717094ebde672642a3886c5fe5622f2b3ed472
adindap-gcp <adindap+demo@google.com>
2023-08-07 16:05:02 +0000

    Add liveness probe

--- src/guestbook/settings.py
+++ src/guestbook/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'guestbook.middleware.HealthCheckMiddleware',
 ]
 
 ROOT_URLCONF = 'guestbook.urls'
